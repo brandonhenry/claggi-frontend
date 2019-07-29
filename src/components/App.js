e3a5cdf1e60e4532ab7f160345ce3c2eaad53baa
bhenry <bhenry@camgian.com>
2019-07-29 12:12:54 -0500

    Added login view

--- src/components/App.js
+++ src/components/App.js
@@ -4,50 +4,19 @@ import { connect } from 'react-redux';
 import { setHasError } from '../actions/windowActions';
 
 import Header from './Header';
-import {withAuth} from "@okta/okta-react";
+import LoginView from "./LoginView";
 
-export default withAuth(class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { authenticated: null };
-        this.checkAuthentication = this.checkAuthentication.bind(this);
-        this.checkAuthentication();
-        this.login = this.login.bind(this);
-        this.logout = this.logout.bind(this);
-    }
-
-    async checkAuthentication() {
-        const authenticated = await this.props.auth.isAuthenticated();
-        if (authenticated !== this.state.authenticated) {
-            this.setState({ authenticated });
-        }
-    }
-
-    componentDidUpdate() {
-        this.checkAuthentication();
-    }
-
-    async login() {
-        // Redirect to '/' after login
-        this.props.auth.login('/');
-    }
-
-    async logout() {
-        // Redirect to '/' after logout
-        this.props.auth.logout('/');
-    }
-
+class App extends Component {
     render() {
         return (
             <div className='app'>
                 <Header />
+                <LoginView />
                 { this.props.children }
-                {this.state.authenticated ?
-                <button onClick={ this.logout}>Logout</button> : <button onClick={this.login}>Login</button> };
             </div>
         );
     }
-})
+}
 
 const mapStateToProps = state =>  ({
     hasError: state.window.hasError
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Route } from 'react-router-dom';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react'
+import AdminDashboard from "./AdminDashboard";
 
 export default class Header extends Component {
     render() {
         return (
             <nav className="navbar navbar-default navbar-static-top">
                 <div className="container">
-                    <div id="navbar-collapse" className="collapse navbar-collapse">
+                    <div>
                         <ul className="nav navbar-nav">
                             <li><Link to="/">Home</Link></li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
+                            <li><Link to="/admin">Dashboard</Link></li>
+                            <li><Route exact path="/" component={Login}/></li>
+                            <li><SecureRoute exact path="/admin" component={AdminDashboard}/></li>
+                            <li><SecureRoute exact path="/implicit/callback" component={ImplicitCallback}/></li>
                         </ul>
                     </div>
                 </div>
