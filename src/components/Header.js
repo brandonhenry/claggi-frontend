51995b8b6f5f556a6fa92d47f13ab229ac83de3d
bhenry <bhenry@camgian.com>
2019-07-31 15:40:15 -0500

    Fixed spacing

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,21 +18,21 @@ class Header extends Component {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                            {
-                                !this.props.authenticated ?
-                                    <>
-                                    <Nav.Link onClick={() => { setView(LOGIN)(store.dispatch, store.getState) } }>Login</Nav.Link>
-                                    <Nav.Link>Register</Nav.Link>
-                                    </>
-                                    :
-                                    <>
-                                    <Nav.Link onClick={() => { setView(DASHBOARD)(store.dispatch, store.getState) }}>Dashboard</Nav.Link>
-                                    <Nav.Link>Listings</Nav.Link>
-                                    <Nav.Link>Orders</Nav.Link>
-                                    <Nav.Link>Settings</Nav.Link>
-                                    <Nav.Link onClick={() => { this.props.auth.logout('/')} }>Logout</Nav.Link>
-                                    </>
-                            }
+                    {
+                        !this.props.authenticated ?
+                            <>
+                            <Nav.Link onClick={() => { setView(LOGIN)(store.dispatch, store.getState) } }>Login</Nav.Link>
+                            <Nav.Link>Register</Nav.Link>
+                            </>
+                            :
+                            <>
+                            <Nav.Link onClick={() => { setView(DASHBOARD)(store.dispatch, store.getState) }}>Dashboard</Nav.Link>
+                            <Nav.Link>Listings</Nav.Link>
+                            <Nav.Link>Orders</Nav.Link>
+                            <Nav.Link>Settings</Nav.Link>
+                            <Nav.Link onClick={() => { this.props.auth.logout('/')} }>Logout</Nav.Link>
+                            </>
+                    }
                     </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
